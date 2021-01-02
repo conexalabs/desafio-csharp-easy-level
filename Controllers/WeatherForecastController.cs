94543347d89924df06658645a9ac7850ec8c8fe6
Leonardo Fleury <fleuryleomoraes@gmail.com>
2021-01-02 14:26:30 -0300

    Alterando o endpoint da API

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace desafio_csharp_easy_level.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/forecast")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
