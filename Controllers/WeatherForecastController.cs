efc6686f50db188ffbd40374999c995bf240f1fa
Leonardo Fleury <fleuryleomoraes@gmail.com>
2021-01-03 20:44:04 -0300

    Comentários

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using desafio_csharp_easy_level.Models;
 using desafio_csharp_easy_level.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace desafio_csharp_easy_level.Controllers
 {
@@ -22,6 +20,17 @@ namespace desafio_csharp_easy_level.Controllers
             _owm = new OpenWeatherMap();
         }
 
+        /// <summary> Retorna a Temperatura atual da cidade requisitada. </summary>
+        /// <remarks> 
+        ///     Realiza uma requizição à API do Open Weather Map para obter a
+        ///     temperatura atual da cidade requisitada.
+        ///     O resultado é então gravado no banco de dados e retornado para
+        ///     o requisitante.
+        /// </remarks>
+        /// <example>
+        ///     GET /api/current/goiania
+        /// </example>
+        /// <param name="name">Nome da cidade buscada.</param>
         [HttpGet("{name}")]
         public async Task<ActionResult<WeatherForecast>> TodayWeatherFromCityName(string name)
         {
@@ -33,6 +42,21 @@ namespace desafio_csharp_easy_level.Controllers
             return CreatedAtAction(nameof(TodayWeatherFromCityName), currentForecast);
         }
 
+        /// <summary> 
+        ///     Retorna a Temperatura atual da cidade mais próxima das coordenadas
+        ///     passadas.
+        /// </summary>
+        /// <remarks> 
+        ///     Realiza uma requizição à API do Open Weather Map para obter a
+        ///     temperatura atual da cidade mais próxima das coordenadas.
+        ///     O resultado é então gravado no banco de dados e retornado para
+        ///     o requisitante.
+        /// </remarks>
+        /// <example>
+        ///     GET /api/current/-16.6,-49.2
+        /// </example>
+        /// <param name="lat">Latitude da coordenada buscada.</param>
+        /// <param name="lon">Longitude da coordenada buscada.</param>
         [HttpGet("{lat},{lon}")]
         public async Task<ActionResult<WeatherForecast>> TodayWeatherFromCityCoordinate(double lat, double lon)
         {
@@ -43,12 +67,13 @@ namespace desafio_csharp_easy_level.Controllers
 
             return CreatedAtAction(nameof(TodayWeatherFromCityCoordinate), currentForecast);
         }
+
     }
 
     [ApiController]
     [Route("api/historic")]
-    public class WeatherForecastHistoricController : ControllerBase
-    {
+    public class WeatherForecastHistoricController: ControllerBase{
+        
         private WeatherContext _context;
         private OpenWeatherMap _owm;
 
@@ -58,13 +83,43 @@ namespace desafio_csharp_easy_level.Controllers
             _owm = new OpenWeatherMap();
         }
 
+        /// <summary> 
+        ///     Retorna uma lista com as Temperatura que foram gravadas
+        ///     anteriormente.
+        /// </summary>
+        /// <remarks> 
+        ///     Encontra todos os registros de temperaturas de uma determinada
+        ///     cidade. 
+        ///     Realiza uma chamada a API do Open Weather Map para corrigir o 
+        ///     nome da cidade buscada.
+        /// </remarks>
+        /// <example>
+        ///     GET /api/historic/goiania
+        /// </example>
+        /// <param name="name">Nome da cidade buscada.</param>
         [HttpGet("{name}")]
         public async Task<ActionResult<IEnumerable<WeatherForecast>>> HistoricWeatherFromCityName(string name)
         {
             string _name = await _owm.GetCityNameByName(name);
             List<WeatherForecast> historicList = _context.WeatherForecasts.Where(f => f.Name == _name).ToList();
-            return historicList;
+            return Ok(historicList);
         }
+
+        /// <summary> 
+        ///     Retorna uma lista com as Temperatura que foram gravadas
+        ///     anteriormente. 
+        /// </summary>
+        /// <remarks> 
+        ///     Encontra todos os registros de temperaturas de uma determinada
+        ///     cidade.
+        ///     Realiza uma chamada a API do Open Weather Map para obter o 
+        ///     nome da cidade mais próxima das coordenas passadas.
+        /// </remarks>
+        /// <example>
+        ///     GET /api/historic/-16.6,-49.2
+        /// </example>
+        /// <param name="lat">Latitude da coordenada buscada.</param>
+        /// <param name="lon">Longitude da coordenada buscada.</param>
 
         [HttpGet("{lat},{lon}")]
         public async Task<ActionResult<IEnumerable<WeatherForecast>>> HistoricWeatherFromCityCoordinate(double lat, double lon)
@@ -73,7 +128,7 @@ namespace desafio_csharp_easy_level.Controllers
             string _name = await _owm.GetCityNameByCoords(lat, lon);
 
             List<WeatherForecast> historicList = _context.WeatherForecasts.Where(f => f.Name == _name).ToList();
-            return historicList;
+            return Ok(historicList);
         }
     }
 }
